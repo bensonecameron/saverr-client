b0f0742e87587a0a341085e77997602a7c608516
Cameron Benson <66973535+bensonecameron@users.noreply.github.com>
2020-09-04 21:08:28 -0400

    fixed some errors working state

--- components/collectionIndex/CollectionIndex.tsx
+++ components/collectionIndex/CollectionIndex.tsx
@@ -1,77 +1,76 @@
-import React, {FormEvent} from 'react';
+import React, {FormEvent} from 'react'
 import sessionToken from '../Topbar'
-import { CollectionType } from '../types/Types';
+import {CollectionType} from '../types/Types'
 import {Link} from 'react-router-dom'
-import { Card, CardHeader, CardTitle, CardBody, Button, Modal, Row, Col } from 'reactstrap';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Button,
+  Modal,
+  Row,
+  Col,
+} from 'reactstrap'
 
 type AcceptedProps = {
   sessionToken: string
+}
+
+type CollectionState = {
   collection: CollectionType
 }
 
-type CollectionProp = {
-  collection: {
-    nameOfCollection: string,
-    descriptionOfCollection: string,
-    impCollection: boolean
-  }
-}
-
-
-
-export default class CollectionIndex extends React.Component <AcceptedProps, CollectionProp> {
-  constructor(props: AcceptedProps){
+export default class CollectionIndex extends React.Component<
+  AcceptedProps,
+  CollectionState
+> {
+  constructor(props: AcceptedProps) {
     super(props)
     this.state = {
-        collection: {
-            nameOfCollection: "",
-            descriptionOfCollection: "",
-            impCollection: false
-
-        }
+      collection: {
+        nameOfCollection: '',
+        descriptionOfCollection: '',
+        impCollection: false,
+      },
     }
   }
 
-  fetchCollection () {
+  fetchCollection() {
     let collection = this.state.collection
     this.setState({collection: collection})
-
 
     fetch('http://localhost:3001/collection/', {
       method: 'GET',
       headers: {
-        'content-type' : 'application/json',
-        'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk5MTc3MDc2LCJleHAiOjE1OTkyNjM0NzZ9.F0_W2zyHyws1qBXBMN2_5yt8mQcPz_YUB1Pwzf4DvaY"
-      }
+        'content-type': 'application/json',
+        authorization: this.props.sessionToken,
+      },
     })
-    .then(res=>res.json())
-    .then(res => {
-      console.log("res:", res);
-      if (res.id) {
-        this.setState({
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('res:', res)
+        if (res.id) {
+          this.setState({
             collection: {
               nameOfCollection: res.nameOfCollection,
               descriptionOfCollection: res.descriptionOfCollection,
-              impCollection: false
-            }
-        })
-      }
-    })
-}
+              impCollection: false,
+            },
+          })
+        }
+      })
+  }
 
-componentWillMount() {
-        
-  this.fetchCollection();
-}
+  componentWillMount() {
+    this.fetchCollection()
+  }
 
-render(){
-  return (
-    <div className="auth-page">
-      <div className="container page">
-        
+  render() {
+    return (
+      <div className="auth-page">
+        <div className="container page"></div>
       </div>
-    </div>
     )
-  
-}  
+  }
 }
